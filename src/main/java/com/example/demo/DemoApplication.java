7ea40cf34437fb55712753e233c3163d83d532d7
Iam20170815 <shifuwang@163.com>
2019-12-07 22:06:07 +0800

    上传springboot项目

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//DataSourceAutoConfiguration会自动加载,如果不想加载数据库就先将其exclude
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
